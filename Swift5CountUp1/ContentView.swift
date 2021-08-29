c416129ceed7e3b50aa17880c8b65fae399938ad
YutaroNakaji <yutaro.nakaji@four-brains.com>
2021-08-29 09:33:23 +0900

    Initial Commit

---
+++ Swift5CountUp1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift5CountUp1
+//
+//  Created by Yutaro Nakaji（B） on 2021/08/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift5CountUp1/Swift5CountUp1App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift5CountUp1App.swift
+//  Swift5CountUp1
+//
+//  Created by Yutaro Nakaji（B） on 2021/08/29.
+//
+
+import SwiftUI
+
+@main
+struct Swift5CountUp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
