a515779ed3cbe1b7cbf474a3f351ce5852dc1a26
YutaroNakaji <yutaro.nakaji@four-brains.com>
2021-08-30 20:33:36 +0900

    commit

--- Swift5CountUp1/ContentView.swift
+++ Swift5CountUp1/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+        }
     }
 }
